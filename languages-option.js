b8da7bf18ed81464a1ce6fbd128367071a8d546a
labib10849 <154999586+labib10849@users.noreply.github.com>
2024-07-06 01:13:53 +0600

    Add files via upload

---
+++ languages-option.js
@@ -0,0 +1,20 @@
+const languages = [
+  {
+    no: "1",
+    name: "English",
+    native: "English",
+    code: "en",
+  },
+  {
+    no: "2",
+    name: "Bangla",
+    native: "Bangla",
+    code: "bn",
+  },
+  {
+    no: "3",
+    name: "German",
+    native: "Deutsch",
+    code: "de",
+  },
+];
